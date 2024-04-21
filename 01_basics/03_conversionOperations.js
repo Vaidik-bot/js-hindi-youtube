f423bca3a89d9d79c9486818bf401ad4086c0b66
Vaidik Gampawar <83026237+Vaidik-bot@users.noreply.github.com>
2024-04-21 09:27:08 +0000

    Conversion of datatypes done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "vaidik"
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = "vaidik"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "vaidik" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
