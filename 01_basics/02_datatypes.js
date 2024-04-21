cc96846fde9dec9956d19ba4c43340ed55f09721
Vaidik Gampawar <83026237+Vaidik-bot@users.noreply.github.com>
2024-04-21 08:59:27 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+//code readablility should be high
+
+let name = "vaidik"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof "vaidik")
+console.log(typeof undefined)
+console.log(typeof null)
