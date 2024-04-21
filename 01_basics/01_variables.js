cb56e445b41023fecf94cc87e0c4781a1b4e67af
Vaidik Gampawar <83026237+Vaidik-bot@users.noreply.github.com>
2024-04-21 06:33:05 +0000

    variables done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,14 @@ const accountId = 12121
 let accountEmail = "vaidik@gmail.com"
 var accountPassword = "0012"
 accountCity = "Chennai"
+let accountState;
+
+// accountId = 3232 // const cannot be changed
+
+accountCity="Bengaluru"
 
 console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
 
